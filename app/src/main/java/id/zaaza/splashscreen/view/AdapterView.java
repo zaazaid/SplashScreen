5426a6633b86a62fb3f2e32fd1dc605da5727004
Deny Apriliansyah <apriliansya@gmail.com>
2023-01-30 14:32:10 +0700

    Initial commit

---
+++ app/src/main/java/id/zaaza/splashscreen/view/AdapterView.java
@@ -0,0 +1,16 @@
+package id.zaaza.splashscreen.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import id.zaaza.splashscreen.R;
+
+public class AdapterView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adapter_view);
+    }
+}
---
+++ app/src/main/java/id/zaaza/splashscreen/view/main/MainActivity.java
@@ -0,0 +1,16 @@
+package id.zaaza.splashscreen.view.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import id.zaaza.splashscreen.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
